a5246a2f47348ceb29da14c86e7f6103a462c960
Aleksi <liukkonenaleksi@gmail.com>
2024-12-03 09:36:40 +0200

    Edit button

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -8,8 +8,10 @@ interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
 
 export function Button({ children, className = '', variant = 'default', size = 'default', ...props }: ButtonProps) {
   return (
-    <button className={`${styles.button} ${styles[variant]} ${styles[`size-${size}`]} ${className}`} {...props}>
-      <span className={styles.button__content}>{children}</span>
+    <button
+      className={`${styles.root} ${styles[variant]} ${styles[`size${size.charAt(0).toUpperCase() + size.slice(1)}`]} ${className}`}
+      {...props}>
+      <span className={styles.content}>{children}</span>
     </button>
   );
 }
